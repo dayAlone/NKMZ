b88bb2213d09378d4ae314a53a8ae295d778c442
dayAlone <andrey.slider@gmail.com>
2015-07-27 18:46:22 +0500

    CSS

--- public_html/local/templates/.default/components/bitrix/catalog.section.list/sections/template.php
+++ public_html/local/templates/.default/components/bitrix/catalog.section.list/sections/template.php
@@ -3,7 +3,7 @@
   <div class="row">
 	  <?foreach($arResult['SECTIONS'] as $section):?>
 	  <div class="col-xs-6 col-sm-3">
-		<a href="<?=$section['SECTION_PAGE_URL']?>" class="section section--<?=$section['ID']?>">
+		<a href="<?=$section['SECTION_PAGE_URL']?>" class="section section--<?=$section['ID']?> <?=(strlen($section['UF_SVG'])==0?"section--no-logo":"")?>">
 			<div style="background-image: url(<?=$section['PICTURE']['SRC']?>)" class="section__image"></div>
             <div class="section__logo"><?=$section['UF_SVG']?></div>
 			<div class="section__name">
