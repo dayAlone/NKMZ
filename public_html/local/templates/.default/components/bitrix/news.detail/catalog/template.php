d2639b1e442348056d9258ef731ef4c34a026848
dayAlone <andrey.slider@gmail.com>
2015-07-27 17:03:48 +0500

    Changes

--- public_html/local/templates/.default/components/bitrix/news.detail/catalog/template.php
+++ public_html/local/templates/.default/components/bitrix/news.detail/catalog/template.php
@@ -27,7 +27,7 @@ $gallery = $arResult["PROPERTIES"]['GALLERY']['VALUE'];
             endforeach;
             ?>
         </div>
-	    <?=html_entity_decode($item['PROPERTIES']['TEXT']['VALUE']['TEXT'])?>
+	    <?=html_entity_decode(nl2br($item['PROPERTIES']['TEXT']['VALUE']['TEXT']))?>
     </div>
   </div>
 </div>
