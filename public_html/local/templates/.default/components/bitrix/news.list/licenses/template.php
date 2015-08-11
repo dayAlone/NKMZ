d2a27fa553e2be84098db092bc8f4ef565f30d01
dayAlone <andrey.slider@gmail.com>
2015-08-11 18:59:44 +0500

    Licencies

--- public_html/local/templates/.default/components/bitrix/news.list/licenses/template.php
+++ public_html/local/templates/.default/components/bitrix/news.list/licenses/template.php
@@ -1,11 +1,10 @@
 <?if(count($arResult['ITEMS'])>0):?>
 <div class="licencies <?=($arResult['SHOW_ALL']?"licencies--show-all":"")?>" <?=($arResult['SHOW_ALL']?"data-pictures='".json_encode($arResult['GALLERY'])."'":"")?>>
 	<?foreach ($arResult['ITEMS'] as $item):
-		?>
-	<a href="#" <?=(!$arResult['SHOW_ALL']?"data-pictures='".json_encode($item["PROPERTIES"]['GALLERY']['VALUE'])."'":"")?> class="licence">
+		?><a href="#" <?=(!$arResult['SHOW_ALL']?"data-pictures='".json_encode($item["PROPERTIES"]['GALLERY']['VALUE'])."'":"")?> class="licence">
 		<div class="licence__image"><img src="<?=$item["PROPERTIES"]['GALLERY']['VALUE'][0]['src']?>"></div>
 		<span class="licence__name"><?=$item['NAME']?></span>
-	</a>
-	<?endforeach;?>
+	</a><?
+	endforeach;?>
 </div>
 <?endif;?>
