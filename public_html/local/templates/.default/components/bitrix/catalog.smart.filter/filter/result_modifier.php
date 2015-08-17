728e07c44675dee3169e6af90003ff41fb423d07
dayAlone <andrey.slider@gmail.com>
2015-08-17 21:34:11 +0500

    CSS

--- public_html/local/templates/.default/components/bitrix/catalog.smart.filter/filter/result_modifier.php
+++ public_html/local/templates/.default/components/bitrix/catalog.smart.filter/filter/result_modifier.php
@@ -4,7 +4,7 @@ function filter_sort($a, $b)
     return strcoll($a['VALUE'], $b['VALUE']);
 }
 foreach ($arResult['ITEMS'] as &$item):
-    if($item['CODE'] != 'PASSAGEWAY')
+    if($item['CODE'] == 'AREA')
         usort($item['VALUES'], "filter_sort");
 endforeach;
 ?>
