2c9b1d30fa3625f182ef8a9c017919a1266bd4ba
dayAlone <andrey.slider@gmail.com>
2015-08-19 17:23:17 +0500

    EN

--- public_html/urlrewrite.php
+++ public_html/urlrewrite.php
@@ -22,19 +22,19 @@ $arUrlRewrite = array(
 		"CONDITION" => "#^/en/catalog/([\\w-_]+)/([\\w-_]+)/.*#",
 		"RULE"      => "&SECTION_CODE=\$1&ELEMENT_CODE=\$2&\$3",
 		"ID"        => "",
-		"PATH"      => "/catalog/index.php",
+		"PATH"      => "/en/catalog/index.php",
 	),
 	array(
 		"CONDITION" => "#^/en/catalog/([\\w-_]+)/.*#",
 		"RULE"      => "&SECTION_CODE=\$1&\$2",
 		"ID"        => "",
-		"PATH"      => "/catalog/index.php",
+		"PATH"      => "/en/catalog/index.php",
 	),
 	array(
 		"CONDITION" => "#^/en/press/([\\w-_]+)/([\\w-_]+)/.*#",
 		"RULE"      => "&SECTION_CODE=\$1&ELEMENT_CODE=\$2&\$3",
 		"ID"        => "",
-		"PATH"      => "/press/index.php",
+		"PATH"      => "/en/press/index.php",
 	),
 	array(
 		"CONDITION" => "#^/ajax/press/([\\w-_]+)/([\\w-_]+)/.*#",
