b4e39e303a9b39a05df3a6ec6080b4ad1ab71ac0
dayAlone <andrey.slider@gmail.com>
2015-09-03 11:52:20 +0500

    News

--- public_html/ajax/press/index.php
+++ public_html/ajax/press/index.php
@@ -4,7 +4,7 @@ $APPLICATION->IncludeComponent("bitrix:news.detail","news",Array(
 	"IBLOCK_ID"     => (strstr($APPLICATION->GetCurDir(),'ru')?2:18),
 	"ELEMENT_CODE"  => $_REQUEST['ELEMENT_CODE'],
 	"CHECK_DATES"   => "N",
-	"IBLOCK_TYPE"   => "content_".strstr($APPLICATION->GetCurDir(),'ru'),
+	"IBLOCK_TYPE"   => "content_".(strstr($APPLICATION->GetCurDir(),'ru')?"ru":"en"),
 	"SET_TITLE"     => "N",
 	"PROPERTY_CODE" => Array("GALLERY"),
 	"CACHE_TYPE"    => "A",
