5b1895f8a37cca775e3dbc4f5860d494ec1476f4
dayAlone <andrey.slider@gmail.com>
2016-08-11 15:08:43 +0700

    Catalog

--- public_html/local/templates/.default/components/bitrix/catalog.section.list/sections/template.php
+++ public_html/local/templates/.default/components/bitrix/catalog.section.list/sections/template.php
@@ -2,7 +2,8 @@
 <div class="sections">
   <?foreach($arResult['SECTIONS'] as $section):?>
 	<a href="<?=$section['SECTION_PAGE_URL']?>" class="section section--<?=$section['ID']?> <?=(strlen($section['UF_SVG'])==0?"section--no-logo":"")?>">
-		<div style="background-image: url(<?=strlen($arParams['PICTURE']) > 0 ? CFile::GetPath($section['DETAIL_PICTURE']) : $section['PICTURE']['SRC']?>)" class="section__image"></div>
+		<div style="background-image: url(<?=strlen($arParams['PICTURE']) > 0 ? CFile::GetPath($section['DETAIL_PICTURE']) : $section['PICTURE']['SRC']?>)" class="section__image section__image--desktop"></div>
+        <div style="background-image: url(<?=CFile::GetPath($section['DETAIL_PICTURE'])?>)" class="section__image section__image--mobile"></div>
         <div class="section__name">
             <div class="section__logo"><?=$section['UF_SVG']?></div>
 			<span><?=$section['~NAME']?></span>
